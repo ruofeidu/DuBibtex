df1ab365b9df18172b154b53d39a885262676c6e
David Li <david@davidl.me>
2024-04-09 21:00:02 +0000

    Refractor test

--- tests/test_find_dois.py
+++ tests/test_find_dois.py
@@ -1,33 +1,32 @@
-import unittest
 import tempfile
 import os
 
 import bibtexparser
+import pytest
 
 from DuBibtex import Parser
 
 
+@pytest.mark.parametrize("filename,correct_doi", [
+    ("signet.bib", "10.1109/ICCV48922.2021.01396"),
+])
+def test_iccv_doi(filename, correct_doi):
+    input_file = os.path.join("tests", "inputs", filename)
 
-class TestFindDOIs(unittest.TestCase):
-
-  
-  def test_iccv_doi(self):
-    filename = "tests/inputs/signet.bib"
-    correct_doi = "10.1109/ICCV48922.2021.01396"
-
-    self.assertTrue(os.path.isfile(filename))
-    library = bibtexparser.parse_file(filename)
+    assert os.path.isfile(input_file), f"File {input_file} does not exist"
+    library = bibtexparser.parse_file(input_file)
 
     with tempfile.NamedTemporaryFile() as fp:
         p = Parser(output_file=fp.name)
         for entry in library.entries:
-          p.copy_from_parsed_entry(entry)
-          p.write_current_item()
+            p.copy_from_parsed_entry(entry)
+            p.write_current_item()
         p.shut_down()
 
         # Check the doi
         generated_library = bibtexparser.parse_file(fp.name)
-        self.assertEqual(len(generated_library.entries), len(library.entries), "Number of entries should be the same")
+        assert len(generated_library.entries) == len(
+            library.entries), "Number of entries should be the same"
         for entry in generated_library.entries:
-          self.assertTrue("doi" in entry)
-          self.assertEqual(entry.fields_dict["doi"].value, correct_doi)
+            assert "doi" in entry
+            assert entry.fields_dict["doi"].value == correct_doi
